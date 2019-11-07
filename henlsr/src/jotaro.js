cd5e608502d11fb7c5e7bbfa7c777fc6a1cf2da8
Mista4444 <mista4444@users.noreply.github.com>
2019-11-06 20:38:27 -0500

    Jotaro Flip
    JOTARO FLIP

--- henlsr/src/jotaro.js
+++ henlsr/src/jotaro.js
@@ -1,15 +1,24 @@
 import React from "react"
+import s from "./Images/jotaros.jpg"
+import p from "./Images/jotarop.jpg"
 
 class Jotaro extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            img: '../Images/jotaros.jpg'
+            img: p
+        }
+    }
+    changeImg = () => {
+        if (this.state.img == p){
+            this.setState({img: s})
+        } else {
+            this.setState({img: p})
         }
     }
 
     render() {
-        return <img src={this.state.img}/>
+        return <img onClick={this.changeImg} src={this.state.img} alt={'big dumb'}/>
     }
 
 }
